3660f9ac63834fee693fe26c70da8d5c03c681bb
jason-yu112 <athena6680@gmail.com>
2025-04-13 15:09:15 -0700

    adding progress 2 to assignment 1

--- assignment1/asg1.js
+++ assignment1/asg1.js
@@ -4,9 +4,11 @@
 // Vertex shader program
 var VSHADER_SOURCE = `
   attribute vec4 a_Position;
+  uniform float u_Size;
   void main() {
   gl_Position = a_Position;
-  gl_PointSize = 10.0;
+  //gl_PointSize = 10.0;
+  gl_PointSize = u_Size;
   }`
 
 // Fragment shader program
@@ -22,6 +24,7 @@ var gl;
 var canvas;
 var a_Position;
 var u_FragColor;
+var u_Size;
 
 
 // Setup WebGL
@@ -58,6 +61,13 @@ function connectVariablestoGLSL() {
     console.log('Failed to get the storage location of u_FragColor');
     return;
   }
+
+  // Get the storage location of u_Size
+  u_Size = gl.getUniformLocation(gl.program, 'u_Size');
+  if (!u_Size) {
+    console.log('Failed to get the storage location of u_Size');
+    return;
+  }
 }
 
 /*================================================ MAIN FUNCTION ==================================================*/
@@ -85,6 +95,7 @@ function main() {
 
 var g_points = [];  // The array for the position of a mouse press
 var g_colors = [];  // The array to store the color of a point
+var g_sizes = []; // The array to store the size of the point
 
 function click(ev) {
   // Convert mouse coordinate to GL
@@ -93,7 +104,17 @@ function click(ev) {
   // Store the coordinates to g_points array
   g_points.push([x, y]);
 
+  // Push the selected color for slider and button
+  g_colors.push(g_selectedColor.slice());
+
+  // Push the selected size of the point
+  g_sizes.push(g_selectedSize);
+
+  // Render the selected shape on canvas
+  renderAllShapes();
+  
   /*
+  OUTDATED CODE
   // Store the coordinates to g_points array
   if (x >= 0.0 && y >= 0.0) {      // First quadrant
     g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
@@ -102,12 +123,10 @@ function click(ev) {
   } else {                         // Others
     g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
   }
-  */
-  // Push the selected color to html
+  
+  // Push the selected color for button
   g_colors.push(g_selectedColor);
-
-  // Render the selected shape on canvas
-  renderAllShapes();
+  */
 }
 
 
@@ -131,28 +150,33 @@ function renderAllShapes() {
   for(var i = 0; i < len; i++) {
     var xy = g_points[i];
     var rgba = g_colors[i];
+    var size = g_sizes[i]
 
     // Pass the position of a point to a_Position variable
     gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
     // Pass the color of a point to u_FragColor variable
     gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+    gl.uniform1f(u_Size, size);
     // Draw
     gl.drawArrays(gl.POINTS, 0, 1);
   }
 } 
 
-let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
 
+let g_selectedColor = [1.0, 1.0, 1.0, 1.0]; // DEFAULT COLOR [WHITE]
+let g_selectedSize=5; // DEFAULT SIZE [5]
 
 // Set up actions for the HTML UI elements
 function addActionsForHtmlUI() {
   // Button Events (Shape Type)
   document.getElementById('green').onclick = function () { g_selectedColor = [0.0, 1.0, 0.0, 1.0]; };
   document.getElementById('red').onclick = function () { g_selectedColor = [1.0, 0.0, 0.0, 1.0]; };
+
+  // Slider Events
   document.getElementById('redSlide').addEventListener('mouseup', function () { g_selectedColor[0] = this.value / 100; });
   document.getElementById('greenSlide').addEventListener('mouseup', function () { g_selectedColor[1] = this.value / 100; });
   document.getElementById('blueSlide').addEventListener('mouseup', function () { g_selectedColor[2] = this.value / 100; });
 
   // Size Slider Events
-  document.getElementById('sizeSlide').addEventListener('mouseup', function () { g_selectedSize = this.value; });
+  document.getElementById('sizeSlide').addEventListener('mouseup', function() {g_selectedSize = this.value; });
 }
