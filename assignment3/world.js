b6f93053bcaf52033974486735609a2457caccdb
jason-yu112 <athena6680@gmail.com>
2025-05-11 17:36:39 -0700

    Prototype1

--- assignment3/world.js
+++ assignment3/world.js
@@ -570,6 +570,138 @@ function renderScene(){
     horn3.render();
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    zDis = zDis -2;
+    xDis = xDis +4;
+
+    var body = new Cube();
+    body.textureNum = 2;
+    body.color = [0.0,1.0,0.0,1.0];
+    body.matrix.translate(xDis+-.35,yDis+-.05,zDis+-.25);
+    body.matrix.rotate(15,0,0,1);
+    body.matrix.rotate(bodyAngle,0.5,0,0);
+    body.matrix.scale(0.9,0.4,0.4);
+    body.renderFast();
+
+    var lleg1 = new Cube();
+    lleg1.textureNum = 2;
+    lleg1.color = [0.0,1.0,0.0,1.0];
+    lleg1.matrix.translate(xDis+0.05,yDis+0.4,zDis+0.15);
+    lleg1.matrix.rotate(g_lleg1Angle,0,0,1);
+    var lleg1Coord = new Matrix4(lleg1.matrix);
+    lleg1.matrix.scale(-0.3,-0.7,0.4);
+    lleg1.renderFast();
+
+    var lleg2 = new Cube();
+    lleg2.textureNum = 2;
+    lleg2.color = [0.0,1.0,0.0,1.0];
+    lleg2.matrix = lleg1Coord;
+    lleg2.matrix.translate(-0.75,-.55,0.05);
+    lleg2.matrix.rotate(g_lleg2Angle,0,0,1);
+    var lleg2Coord = new Matrix4(lleg2.matrix);
+    lleg2.matrix.scale(-0.2,-0.6,0.3);
+    lleg2.renderFast();
+
+    var lfoot = new Cube();
+    lfoot.textureNum = 2;
+    lfoot.color = [0.0,1.0,0.0,1.0];
+    lfoot.matrix = lleg2Coord;
+    lfoot.matrix.translate(-0.10,.10,-0.05);
+    lfoot.matrix.rotate(g_lfootAngle,0,0,1);
+    lfoot.matrix.scale(0.5,0.1,0.4);
+    lfoot.renderFast();
+    
+    var rleg1 = new Cube();
+    rleg1.textureNum = 2;
+    rleg1.color = [0.0,1.0,0.0,1.0];
+    rleg1.matrix.translate(xDis+0.05,yDis+0.4,zDis+-0.65);
+    rleg1.matrix.rotate(g_rleg1Angle,0,0,1);
+    var rleg1Coord = new Matrix4(rleg1.matrix);
+    rleg1.matrix.scale(-0.3,-0.7,0.4);
+    rleg1.renderFast();
+
+    var rleg2 = new Cube();
+    rleg2.textureNum = 2;
+    rleg2.color = [0.0,1.0,0.0,1.0];
+    rleg2.matrix = rleg1Coord;
+    rleg2.matrix.translate(-0.75,-.55,0.05);
+    rleg2.matrix.rotate(g_rleg2Angle,0,0,1);
+    var rleg2Coord = new Matrix4(rleg2.matrix);
+    rleg2.matrix.scale(-0.2,-0.6,0.3);
+    rleg2.renderFast();
+
+    var rfoot = new Cube();
+    rfoot.textureNum = 2;
+    rfoot.color = [0.0,1.0,0.0,1.0];
+    rfoot.matrix = rleg2Coord;
+    rfoot.matrix.translate(-0.10,.10,-0.05);
+    rfoot.matrix.rotate(g_lfootAngle,0,0,1);
+    rfoot.matrix.scale(0.5,0.1,0.4);
+    rfoot.renderFast();
+
+    var tail1 = new Cube();
+    tail1.textureNum = 2;
+    tail1.color = [0.0,1.0,0.0,1.0];
+    tail1.matrix.translate(xDis+-.3,yDis+0,zDis+-.2);
+    tail1.matrix.rotate(-tail1ZAngle,0,0.5,0);
+    tail1.matrix.rotate(-g_tail1Angle,0,0,1);
+    var tail1Coord = new Matrix4(tail1.matrix);
+    tail1.matrix.scale(-0.6,0.3,0.3);
+    tail1.renderFast();
+
+    var tail2 = new Cube();
+    tail2.textureNum = 2;
+    tail2.color = [0.0,1.0,0.0,1.0];
+    tail2.matrix = tail1Coord;
+    tail2.matrix.translate(-0.55,.05,0.05);
+    tail2.matrix.rotate(-tail2ZAngle,0,0.5,0);
+    tail2.matrix.rotate(-g_tail2Angle,0,0,1);
+    tail2.matrix.scale(-0.4,0.2,0.2);
+    tail2.renderFast();
+
+    var head1 = new Cube();
+    head1.textureNum = 2;
+    head1.color = [0.0,1.0,0.0,1.0];
+    head1.matrix.translate(xDis+.35,yDis+.45,zDis+-.35);
+    head1.matrix.rotate(g_head1Angle,0,.5,0);
+    head1.matrix.rotate(-g_head2YAngle,0,0,1);
+    head1.matrix.scale(0.6,0.2,0.6);
+    head1.renderFast();
+
+    var head2 = new Cube();
+    head2.textureNum = 2;
+    head2.color = [0.0,1.0,0.0,1.0];
+    head2.matrix.translate(xDis+0.35,yDis+.6,zDis+-.35);
+    head2.matrix.rotate(g_head2Angle,0,0.5,0);
+    head2.matrix.rotate(g_head2YAngle,0,0,1);
+    var head2Coord = new Matrix4(head2.matrix);
+    head2.matrix.scale(0.6,0.3,0.6);
+    head2.renderFast();
+
+    
+    var horn1 = new Cone();
+    horn1.textureNum = 2;
+    horn1.color = [.9,.9,.9];
+    horn1.matrix = head2Coord;
+    horn1.matrix.translate(0.15,.30,0.3);
+    horn1.matrix.rotate(-90,1,0,0);
+    horn1.matrix.scale(0.05,0.05,0.05);
+    horn1.render();
+
+    var horn2 = new Cone();
+    horn2.textureNum = 2;
+    horn2.color = [.9,.9,.9];
+    horn2.matrix = head2Coord;
+    horn2.matrix.translate(2.35,.0,0.0);
+    horn2.render();
+
+    var horn3 = new Cone();
+    horn3.textureNum = 2;
+    horn3.color = [.9,.9,.9];
+    horn3.matrix = head2Coord;
+    horn3.matrix.translate(2.35,.0,0.0);
+    horn3.render();
+
     var duration = performance.now() - startTime;
     if((Math.floor(10000/duration)/10) < lowestFPS){
         lowestFPS = Math.floor(10000/duration)/10;
